ca2b63474968047afcc9ed0b1d2abf4b5eae03e6
Keo222 <kyote222@gmail.com>
2021-12-22 11:09:00 -0800

    added in app.listen

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const express = require("express");
 
+const PORT = process.env.PORT || 3001;
+
 const app = express();
 
 app.use(express.static(path.resolve(__dirname, '../client/build')));
@@ -11,4 +13,8 @@ app.get('/api', (req, res) => {
 
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+})
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`)
 })
