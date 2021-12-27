447b50b0670c7e55874ab0d31dc8a15947a733f9
Keo222 <kyote222@gmail.com>
2021-12-26 19:51:39 -0800

    added new node module

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,9 @@
-import React, {useState, useEffect} from 'react'
+import React from 'react'
 
 function App() {
-  const [data, setData] = useState(null);
-
-  useEffect(() => {
-    fetch("/api")
-    .then((res) => res.json())
-    .then((data) => setData(data.message));
-  }, []);
-
   return (
-    <div className='App'>
-      <header className="App-header">
-        <p>{!data ? "Loading..." : data}</p>
-      </header>
+    <div>
+      <h1>Hello World</h1>
     </div>
   )
 }
