1ab138eaedd32369d3dee746519eaff42d0e0439
Keo222 <kyote222@gmail.com>
2022-07-07 13:21:10 -0700

    checkout page to TS

--- client/src/pages/Checkout.tsx
+++ client/src/pages/Checkout.tsx
@@ -6,7 +6,17 @@ import { PageDiv } from "../StyledElements/divs";
 // Components
 import ShoppingCart from "../components/ShoppingCart";
 
-function Checkout({ art, cart, setCart, updateCartQty }) {
+// Types
+import { TSaleArt, TCartItem } from "src/types/artInfoTypes";
+
+type Props = {
+  art: TSaleArt[];
+  cart: TCartItem[];
+  setCart: React.Dispatch<React.SetStateAction<TCartItem[]>>;
+  updateCartQty: (newId: string, newQty: number) => void;
+};
+
+function Checkout({ art, cart, setCart, updateCartQty }: Props) {
   return (
     <PageDiv>
       <ShoppingCart
